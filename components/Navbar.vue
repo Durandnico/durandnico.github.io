39a3c2a147a075e8456a9e4829742591487c8111
durandnico <nicod4956@gmail.com>
2023-08-20 23:39:16 +0200

    main page bullshit

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,41 +1,44 @@
 <template>
-    <nav>
+    <nav class="header-nav">
         <v-toolbar flat app dark color="#222222">
             <span class="title pl-10 font-weight-bold c-white">DURAND Nicolas</span>
             
             <v-spacer></v-spacer>
             <v-tabs color="#07a70f" dark class="hidden-sm-and-down -ml-64">
                 <v-tab :value="1" @click="navigateTo('/')" class="pr-6 ml-1 mr-1">Home</v-tab>
+                <v-tab :value="2" @click="navigateTo('/cv')" class="pa-6 ml-1 mr-1 c-white">Cv</v-tab>
                 <v-tab :value="3" @click="navigateTo('/projects')" class="pa-6 ml-1 mr-1 c-white">projects</v-tab>
-                <v-tab :value="2" @click="navigateTo('/about')" class="pa-6 ml-1 mr-16 c-white">about</v-tab>
+                <v-tab :value="4" @click="navigateTo('/about')" class="pa-6 ml-1 mr-16 c-white">about</v-tab>
             </v-tabs>
             <v-spacer :class="{hidden: width < 1600 }"></v-spacer>
             
             <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
         </v-toolbar>
 
-        <v-navigation-drawer app v-model="drawer" class="hidden-md-and-up">
+        <v-navigation-drawer app dark color="#222222" v-model="drawer" class="hidden-md-and-up">
             <v-list-item class=ma-3>
-                <v-list-item-content>
                     <v-list-item-title class="title font-weight-bold">
-                        Durand Nicolas {{test}}
+                        Durand Nicolas
                     </v-list-item-title>
-                </v-list-item-content>
             </v-list-item>
 
             <v-divider></v-divider>
             
                 <v-list nav>
                         <v-list-item @click="{drawer = false; navigateTo('/'); test=1}" class="hv">
-                            <span class="ml-6 font-500">home</span>
+                            <span class="pa-4 ml-2 pr-32">home</span>
                         </v-list-item>
 
-                        <v-list-item @click="{drawer = false; navigateTo('/projects'); test=3}" class="hv">
-                            <span :value="3" class="pa-4 ml-2 pr-32">projects</span>
+                        <v-list-item @click="{drawer = false; navigateTo('/cv'); test=2}" class="hv">
+                            <span class="pa-4 ml-2 pr-32">CV</span>
                         </v-list-item>
-                       
-                        <v-list-item  @click="{drawer = false; navigateTo('/projects'); test=2}" class="hv">
-                            <span :value="2" class="pa-4 ml-2 pr-32">about</span>
+
+                        <v-list-item @click="{drawer = false; navigateTo('/projects'); test=2}" class="hv">
+                            <span class="pa-4 ml-2 pr-32">projects</span>
+                        </v-list-item>
+
+                        <v-list-item  @click="{drawer = false; navigateTo('/about'); test=3}" class="hv">
+                            <span class="pa-4 ml-2 pr-32">about</span>
                         </v-list-item>
 
                 </v-list>
@@ -51,10 +54,19 @@ const drawer = ref(false);
 const test = ref(null)
 
 
+
+
 </script>
 
 <style scoped>
 
+.header-nav {
+    z-index: 99;
+}
+
+.header-nav * {
+    z-index: 99;
+}
 .title {
     font-size: 1.25rem;
     font-weight: 400;
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,208 @@
 <template>
-    <div>
-        <p>Hey !</p>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, voluptas. Possimus consequuntur cum magni totam quam mollitia vero asperiores rerum sit vitae praesentium atque error, unde corrupti perferendis nemo facilis eos dolorem fuga optio maxime dicta ea quae! Voluptates expedita fuga eveniet? Laudantium commodi saepe a dolorum magni iste inventore quis nam asperiores! Cum facere consequuntur distinctio minus laborum. Repellat, eum? At exercitationem natus, eveniet possimus cupiditate repellendus aperiam nam dolorum hic labore repellat numquam atque eum odit quidem sint corrupti, expedita fuga debitis qui laboriosam doloribus! Eos dolores soluta, ipsa quis itaque quae quo, ullam hic sed quibusdam perspiciatis laudantium saepe tenetur iure esse. Cum veniam iure laborum molestiae similique! Amet aspernatur itaque magnam velit aut est, consectetur rerum non enim ipsa in ipsam sit iure voluptates. Ipsam dicta aspernatur porro quibusdam. Deleniti, voluptas aliquid. Libero, inventore voluptatibus officiis facere minima magni! Alias suscipit, excepturi voluptatum, a, minus nostrum fugit odio consectetur laudantium sit quod doloremque illum rem adipisci delectus saepe vero reprehenderit. Quidem, fugiat, sint totam voluptatibus est illo temporibus, eius asperiores impedit odit commodi nam veritatis! Sed magni rerum quae iste, et unde vitae? Adipisci numquam sint cum blanditiis mollitia magni facilis ipsa nulla dicta. Illo eos doloremque ab, possimus sit nemo impedit enim numquam labore laboriosam reprehenderit rerum iste quia magnam obcaecati adipisci, ducimus blanditiis. Magni minima quaerat labore atque cumque molestiae alias sit omnis? Repellat quod quaerat velit numquam dicta harum minima assumenda asperiores! Ratione expedita, facere repudiandae sed cumque possimus asperiores deleniti recusandae eum?</p>
+    <div class="home">
+        <section class="nav">
+            <h1>Durand Nicolas</h1>
+            <h3 class="span loader">
+                <span class="m">C</span>
+                <span class="m">o</span>
+                <span class="m">d</span>
+                <span class="m">i</span>
+                <span class="m">n</span>
+                <span class="m">g</span>
+                <span class="m">&nbsp;</span>
+                <span class="m">N</span>
+                <span class="m">'</span>
+                <span class="m">&nbsp;</span>
+                <span class="m">R</span>
+                <span class="m">u</span>
+                <span class="m">n</span>
+                <span class="m">n</span>
+                <span class="m">i</span>
+                <span class="m">n</span>
+                <span class="m">g</span>
+                <span class="m">&nbsp;</span>
+                <span class="m">4</span>
+                <span class="m">&nbsp;</span>
+                <span class="m">L</span>
+                <span class="m">i</span>
+                <span class="m">f</span>
+                <span class="m">e</span>
+            </h3>
+        </section>
     </div>
 </template>
 
-<script setup lang="ts">
 
-</script>
+
 
 <style scoped>
 
+.home {
+    position: absolute;
+    width: 100vw;
+    font-family: "ROBOTO", sans-serif;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+.nav {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 93.14vh;
+  width: 100vw;
+  background-color: #1e1f26;
+  text-align: center;
+  padding: 0 2em;
+}
+
+.nav h1,
+.slider h1 {
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 5vw;
+  margin: 0;
+  padding-bottom: 0.5rem;
+  letter-spacing: 0.5rem;
+  color: #03dac6;
+  transition: all 0.3s ease;
+  z-index: 3;
+}
+h1:hover {
+  transform: translate3d(0, -10px, 22px);
+  color: #ff0266;
+}
+
+h3.span {
+  font-size: 2vw;
+  letter-spacing: 0.7em;
+  font-family: "ROBOTO", sans-serif;
+  font-weight: 300;
+  color: #faebd7;
+  z-index: 4;
+}
+span:hover {
+  color: #ff0266;
+  font-weight: 500;
+  font-size: 2.2vw;
+}
+
+a {
+  text-decoration: none;
+}
+
+@media (min-width: 800px) {
+  .nav h1,
+  .slider h1 {
+    font-size: 5vw;
+  }
+
+  .nav h2,
+  .slider h2 {
+    font-size: 3vw;
+  }
+
+  .nav-tab {
+    font-size: 3vw;
+  }
+}
+
+@media screen only (min-width: 360px) {
+  .nav h1,
+  .slider h1 {
+    font-size: 8vw;
+  }
+
+  .nav h2,
+  .slider h2 {
+    font-size: 2vw;
+    letter-spacing: 0.2vw;
+  }
+
+  .nav-tab {
+    font-size: 1.2vw;
+  }
+}
+
+.loader span {
+  color: #faebd7;
+  text-shadow: 0 0 0 #faebd7;
+  -webkit-animation: loading 1s ease-in-out infinite alternate;
+}
+
+@-webkit-keyframes loading {
+  to {
+    text-shadow: 20px 0 70px #ff0266;
+    color: #ff0266;
+  }
+}
+.loader span:nth-child(2) {
+  -webkit-animation-delay: 0.1s;
+}
+.loader span:nth-child(3) {
+  -webkit-animation-delay: 0.2s;
+}
+.loader span:nth-child(4) {
+  -webkit-animation-delay: 0.3s;
+}
+.loader span:nth-child(5) {
+  -webkit-animation-delay: 0.4s;
+}
+.loader span:nth-child(6) {
+  -webkit-animation-delay: 0.5s;
+}
+.loader span:nth-child(7) {
+  -webkit-animation-delay: 0.6s;
+}
+.loader span:nth-child(8) {
+  -webkit-animation-delay: 0.7s;
+}
+.loader span:nth-child(9) {
+  -webkit-animation-delay: 0.8s;
+}
+
+.loader span:nth-child(10) {
+  -webkit-animation-delay: 0.9s;
+}
+.loader span:nth-child(11) {
+  -webkit-animation-delay: 1s;
+}
+.loader span:nth-child(12) {
+  -webkit-animation-delay: 1.1s;
+}
+.loader span:nth-child(13) {
+  -webkit-animation-delay: 1.2s;
+}
+.loader span:nth-child(14) {
+  -webkit-animation-delay: 1.3s;
+}
+.loader span:nth-child(15) {
+  -webkit-animation-delay: 1.4s;
+}
+.loader span:nth-child(16) {
+  -webkit-animation-delay: 1.5s;
+}
+.loader span:nth-child(17) {
+  -webkit-animation-delay: 1.6s;
+}
+.loader span:nth-child(18) {
+  -webkit-animation-delay: 1.7s;
+}
+.loader span:nth-child(19) {
+  -webkit-animation-delay: 1.8s;
+}
+.loader span:nth-child(20) {
+  -webkit-animation-delay: 1.9s;
+}
+.loader span:nth-child(21) {
+  -webkit-animation-delay: 2s;
+}
+.loader span:nth-child(22) {
+  -webkit-animation-delay: 2.1s;
+}
+
 </style>
+
+
